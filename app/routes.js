a1e1ff912c84a26c97558d588ab68402e4e4bade
Shreyas Bhat <shrysbhat@gmail.com>
2016-04-18 14:42:58 -0700

    Hungry Hacker Challenge - 1st & 2nd phase
    1. A working Node app, replaced todo with food. Successful POST requests
    that store food_name & price in the MongoDB collection.
    
    Changes in the Mongo model -
    food_name : String,
    price : Number

---
+++ app/routes.js
@@ -0,0 +1,71 @@
+var Todo = require('./models/todo');
+
+function getTodos(res) {
+    Todo.find(function (err, todos) {
+
+        // if there is an error retrieving, send the error. nothing after res.send(err) will execute
+        if (err) {
+            res.send(err);
+        }
+
+        res.json(todos); // return all todos in JSON format
+    });
+}
+;
+
+module.exports = function (app) {
+
+    // api ---------------------------------------------------------------------
+    // get all todos
+    app.get('/api/food', function (req, res) {
+        // use mongoose to get all todos in the database
+        console.log('hi');
+        getTodos(res);
+        
+    });
+
+    // create todo and send back all todos after creation
+    app.post('/api/food', function (req, res) {
+
+        console.log('food - ' + req.body.food_name);
+        console.log('price - ' + req.body.price);
+        // create a todo, information comes from AJAX request from Angular
+        Todo.create({
+            food_name: req.body.food_name,
+            price: req.body.price
+//            done: false
+        }, function (err, todo) {
+            if (err)
+                res.send(err);
+
+            // get and return all the todos after you create another
+            getTodos(res);
+        });
+
+    });
+
+    // delete a todo
+    app.delete('/api/food/:food_id', function (req, res) {
+        Todo.remove({
+            _id: req.params.food_name
+        }, function (err, todo) {
+            if (err)
+                res.send(err);
+
+            getTodos(res);
+        });
+    });
+
+    // get sum of food prices
+    app.get('/api/total', function (req, res) {
+           
+    });
+    
+    // application -------------------------------------------------------------
+    app.get('*', function (req, res) {
+        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    });
+    
+        
+    
+};
