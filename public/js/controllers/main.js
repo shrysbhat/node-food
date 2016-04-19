a0a39a7c16f548eef24a0545fdfc429acf85c022
Shreyas Bhat <shrysbhat@gmail.com>
2016-04-18 22:11:49 -0700

    Working with AngularJS - 2
    1. Add a new food item from homepage.
    2. http://localhost:8080/api/total - gives the total taxed total of all
    the food items in the database

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,7 +1,7 @@
 angular.module('foodController', [])
 
 	// inject the Todo service factory into our controller
-	.controller('mainController', ['$scope','$http', 'Foods', function($scope, $http, Foods) {
+	.controller('mainController', ['$scope', '$http', 'Foods', function ($scope, $http, Foods) {
 		$scope.formData = {};
 		$scope.loading = true;
 
@@ -9,41 +9,42 @@ angular.module('foodController', [])
 		// when landing on the page, get all todos and show them
 		// use the service to get all the todos
         Foods.get()
-            .success(function(data) {
+            .success(function (data) {
                 $scope.foods = data;
                 $scope.loading = false;
             });
 
 		// CREATE ==================================================================
 		// when submitting the add form, send the text to the node API
-		$scope.createFood = function() {
+		$scope.createFood = function () {
 
 			// validate the formData to make sure that something is there
 			// if form is empty, nothing will happen
-			if ($scope.formData.food_name != undefined || $scope.formData.price != undefined) {
+			if ($scope.formData.food_name !== undefined || $scope.formData.price !== undefined) {
 				$scope.loading = true;
 
 				// call the create function from our service (returns a promise object)
 				Foods.create($scope.formData)
 
 					// if successful creation, call our get function to get all the new todos
-					.success(function(data) {
+					.success(function (data) {
 						$scope.loading = false;
-						$scope.formData = {}; // clear the form so our user is ready to enter another
-						$scope.food.food_name = data; // assign our new list of todos
-                        $scope.food.price = data; // assign our new list of todos
+//						$scope.food.food_name = $scope.formData.food_name; // assign our new list of todos
+//                        $scope.food.price = $scope.formData.price; // assign our new list of todos
+                        $scope.formData = {}; // clear the form so our user is ready to enter another
+                        $scope.foods = data;
 					});
 			}
 		};
 
 		// DELETE ==================================================================
 		// delete a todo after checking it
-		$scope.deleteFood = function(food_name) {
+		$scope.deleteFood = function (food_name) {
 			$scope.loading = true;
 
 			Foods.delete(food_name)
 				// if successful creation, call our get function to get all the new todos
-				.success(function(data) {
+				.success(function (data) {
 					$scope.loading = false;
 					$scope.foods = data; // assign our new list of todos
 				});
--- public/js/services/foods.js
+++ public/js/services/foods.js
@@ -2,15 +2,15 @@ angular.module('foodService', [])
 
 	// super simple service
 	// each function returns a promise object 
-	.factory('Foods', ['$http',function($http) {
+	.factory('Foods', ['$http', function ($http) {
 		return {
-			get : function() {
+			get : function () {
 				return $http.get('/api/food');
 			},
-			create : function(foodData) {
+			create : function (foodData) {
 				return $http.post('/api/food', foodData);
 			},
-			delete : function(food_name) {
+			delete : function (food_name) {
 				return $http.delete('/api/food/' + food_name);
 			}
 		}
