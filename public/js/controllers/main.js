26d3dbd0b3c417d9b1c544142d4b3c495d71670f
Shreyas Bhat <shrysbhat@gmail.com>
2016-04-18 21:48:27 -0700

    Working with AngularJS
    1. Corrected .js files to get the existing 'food's in the Mongo
    collection
    
    2. Delete a food item working properly from the homepage

--- public/js/controllers/main.js
+++ public/js/controllers/main.js
@@ -1,18 +1,18 @@
 angular.module('foodController', [])
 
 	// inject the Todo service factory into our controller
-	.controller('mainController', ['$scope','$http','Foods', function($scope, $http, Foods) {
+	.controller('mainController', ['$scope','$http', 'Foods', function($scope, $http, Foods) {
 		$scope.formData = {};
 		$scope.loading = true;
 
 		// GET =====================================================================
 		// when landing on the page, get all todos and show them
 		// use the service to get all the todos
-		Foods.get()
-			.success(function(data) {
-				$scope.foods = data;
-				$scope.loading = false;
-			});
+        Foods.get()
+            .success(function(data) {
+                $scope.foods = data;
+                $scope.loading = false;
+            });
 
 		// CREATE ==================================================================
 		// when submitting the add form, send the text to the node API
@@ -20,7 +20,7 @@ angular.module('foodController', [])
 
 			// validate the formData to make sure that something is there
 			// if form is empty, nothing will happen
-			if ($scope.formData.text != undefined) {
+			if ($scope.formData.food_name != undefined || $scope.formData.price != undefined) {
 				$scope.loading = true;
 
 				// call the create function from our service (returns a promise object)
@@ -30,17 +30,18 @@ angular.module('foodController', [])
 					.success(function(data) {
 						$scope.loading = false;
 						$scope.formData = {}; // clear the form so our user is ready to enter another
-						$scope.foods = data; // assign our new list of todos
+						$scope.food.food_name = data; // assign our new list of todos
+                        $scope.food.price = data; // assign our new list of todos
 					});
 			}
 		};
 
 		// DELETE ==================================================================
 		// delete a todo after checking it
-		$scope.deleteFood = function(id) {
+		$scope.deleteFood = function(food_name) {
 			$scope.loading = true;
 
-			Foods.delete(id)
+			Foods.delete(food_name)
 				// if successful creation, call our get function to get all the new todos
 				.success(function(data) {
 					$scope.loading = false;
--- public/js/services/foods.js
+++ public/js/services/foods.js
@@ -2,16 +2,16 @@ angular.module('foodService', [])
 
 	// super simple service
 	// each function returns a promise object 
-	.factory('foods', ['$http',function($http) {
+	.factory('Foods', ['$http',function($http) {
 		return {
 			get : function() {
-				return $http.get('/api/foods');
+				return $http.get('/api/food');
 			},
 			create : function(foodData) {
-				return $http.post('/api/foods', foodData);
+				return $http.post('/api/food', foodData);
 			},
-			delete : function(id) {
-				return $http.delete('/api/foods/' + id);
+			delete : function(food_name) {
+				return $http.delete('/api/food/' + food_name);
 			}
 		}
 	}]);
